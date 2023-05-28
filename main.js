01968be6185330ed9fe6b869a751a634605a94b0
Muertoparado <nicolas.casanova20@gmail.com>
2023-05-27 21:16:29 -0500

    feat(taller 2): :rocket: ejercicio 2

--- main.js
+++ main.js
@@ -1,15 +1,18 @@
-let myform = document.querySelector("#myForm"); //seleccionamos el formulario
-myform.addEventListener("submit", async (e) => { //creamos un evento de escucha al formulario de un "Submit"
-    e.preventDefault(); //prevenimos el comportamiento por defecto del formulario al hacer un submit
-    let myHeaders = new Headers({ "Content-Type": "application/json" }) //creamos el header que vamos a incluir en el fetch
-    let data = Object.fromEntries(new FormData(e.target)); //recuperamos los datos que se llenan en el form
-    let config = { //creamos la configuracion que vamos a pasar al fetch
-        method: "POST",
-        headers: myHeaders,
-        body: JSON.stringify(data)
-    };
-    let res = await (await fetch("../model/api.php", config)).text(); //realizamos el fetch y transformamos la respuesta a txt
-    document.querySelector("pre").innerHTML = res; //insertamos la respuesta en la etiqueta <pre></pre>
+//php -S localhost:3000
+
+
+let ej1 = document.querySelector("#ejercicio1"); //seleccionamos el formulario
+    ej1.addEventListener("submit", async (e) => { //creamos un evento de escucha al formulario de un "Submit"
+        e.preventDefault(); //prevenimos el comportamiento por defecto del formulario al hacer un submit
+        let myHeaders = new Headers({ "Content-Type": "application/json" }) //creamos el header que vamos a incluir en el fetch
+        let data = Object.fromEntries(new FormData(e.target)); //recuperamos los datos que se llenan en el form
+        let config = { //creamos la configuracion que vamos a pasar al fetch
+            method: "POST",
+            headers: myHeaders,
+            body: JSON.stringify(data)
+        };
+        let res = await (await fetch("model/api.php", config)).text(); //realizamos el fetch y transformamos la respuesta a txt
+        document.querySelector("#ej1").innerHTML = res; //insertamos la respuesta en la etiqueta <pre></pre>
 })
 
 let ej2 = document.querySelector("#ejercicio2");
---
+++ model/api2.php
@@ -0,0 +1,33 @@
+<?php
+header("Content-Type: application/json"); 
+$_DATA = json_decode(file_get_contents("php://input"), true);
+$METHOD = $_SERVER["REQUEST_METHOD"];
+var_dump($_DATA);
+switch($METHOD) {
+    case "POST":
+    foreach($_DATA as $key => $value){
+        if(!is_numeric($value)){
+            $res=[
+                "numero"=>"Dato no mumerico (Error)"
+            ];
+        }else{
+            $res=[
+                "numero"=>parImpar($value)
+            ];
+        }
+    }
+}
+function parImpar($num){
+    if($num>=0){
+         if($num%2==0){
+            return 'par';
+        }else{
+            return 'impar';
+        }
+    }else{
+        return 'El dato a evaluar no puede ser negativo';
+    }
+}
+echo  json_encode($res, JSON_PRETTY_PRINT);
+
+?>
